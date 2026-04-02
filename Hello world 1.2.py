907efeaeadaa3afc90fc1eb4dcc821e327b2b0d9
phytonrinat-stack <phytonrinat@gmail.com>
2026-04-02 15:14:33 +0400

    Add simple math quiz in Hello world 1.2.py

---
+++ Hello world 1.2.py
@@ -0,0 +1,8 @@
+import random
+print("Hello world 1.2")
+a=random.randint(1,20)
+b=random.randint(1,20)
+cavab=a+b
+cavab2=input(f"how much {a}+{b}? ")
+if cavab==int(cavab2):     print("True")
+else:     print("False")
